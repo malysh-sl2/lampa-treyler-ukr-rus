31408d2a8699e77f05c7fb01bc43c032de4dbff5
malysh-sl2 <malyshsl@yahoo.com>
2025-03-21 21:34:29 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -3,9 +3,11 @@ Lampa.Plugins.add({
     version: "1.0",
     description: "Додає трейлери з YouTube українською та російською",
     onStart: function() {
-        console.log('Plugin started');
+        console.log('Плагін YouTube Трейлери запущено');
         Lampa.Listener.follow('full', function(e) {
+            console.log('Подія full отримана', e);
             if (e.type === 'complete') {
+                console.log('Подія complete обробляється');
                 let movieTitle = e.data.movie.title;
                 let youtubeSearchURL = `https://www.youtube.com/results?search_query=${encodeURIComponent(movieTitle + " трейлер українською")}`;
 
@@ -17,6 +19,7 @@ Lampa.Plugins.add({
                     `,
                     onSelect: function(element) {
                         let url = element.getAttribute("data-url");
+                        console.log('Вибір трейлера:', url);
                         Lampa.Activity.push({
                             url: url,
                             title: "Трейлер",
