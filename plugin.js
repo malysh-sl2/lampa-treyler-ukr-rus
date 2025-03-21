c9ef21467916e3e41c21092305325ee1476c0363
malysh-sl2 <malyshsl@yahoo.com>
2025-03-21 12:47:20 +0300

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,29 @@
+Lampa.Plugins.add({
+    title: "YouTube Трейлери",
+    version: "1.0",
+    description: "Додає трейлери з YouTube українською та російською",
+    onStart: function() {
+        Lampa.Listener.follow('full', function(e) {
+            if (e.type === 'complite') {
+                let movieTitle = e.data.movie.title;
+                let youtubeSearchURL = `https://www.youtube.com/results?search_query=${encodeURIComponent(movieTitle + " трейлер українською")}`;
+
+                Lampa.Modal.open({
+                    title: "Виберіть трейлер",
+                    html: `
+                        <button class="trailer-btn" data-url="${youtubeSearchURL}">Трейлер українською</button>
+                        <button class="trailer-btn" data-url="https://www.youtube.com/results?search_query=${encodeURIComponent(movieTitle + " трейлер російською")}">Трейлер російською</button>
+                    `,
+                    onSelect: function(element) {
+                        let url = element.getAttribute("data-url");
+                        Lampa.Activity.push({
+                            url: url,
+                            title: "Трейлер",
+                            component: "browser"
+                        });
+                    }
+                });
+            }
+        });
+    }
+});
